8618a24119816bf130cdccfaaa0e2b0cebc6c739
unknown <luka.koscak96@gmail.com>
2020-03-30 14:22:27 +0200

    Initial deployment ready

---
+++ server/build/precache-manifest.882744ba168f25ed7bc367790405dad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db774b5dcba1089aed409845a12b4d4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d779fe0fabf6ca2730",
+    "url": "/static/js/2.acb3084f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.acb3084f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8e91fb536edd805191",
+    "url": "/static/js/main.c3f72776.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -6,8 +6,6 @@ const path = require('path');
 const connection = require('./database/config');
 const transactions = require('./routes/transactions');
 
-
-
 const app = express();
 app.use(express.json());
 
@@ -16,6 +14,13 @@ connection();
 
 // API routes for transactions
 app.use("/api/v1/transactions",transactions);
+
+// Statitc folder config
+app.use(express.static(path.join(__dirname,'build')));
+
+app.get('*',(req,res,next) => {
+    res.sendFile(path.join(__dirname,'build','index.html'));
+});
 
 app.listen(process.env.PORT, 
     console.log(`Server started in ${process.env.node_env} mode at port ${process.env.PORT || 5000}`));
