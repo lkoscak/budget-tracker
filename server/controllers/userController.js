3446edae14a4c71e1def43af54f22d011a02b3e7
unknown <luka.koscak96@gmail.com>
2020-03-31 15:43:00 +0200

    Login functionality finished

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -59,10 +59,64 @@ module.exports.registerUser = async (req,res,next) => {
                                 });
 
                                 });
-                            
                         }
                     })
                 });
+            }
+        }
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            error:error.message
+        });
+    }
+};
+
+// @desc    Login - authenticate user
+// @route   POST api/v1/users/login
+// @access  Public
+module.exports.loginUser = async (req,res,next) => {
+    try {
+        const {email, password} = req.body;
+        if(email===""||password==="") {
+            res.status(400).json({
+                success:false,
+                error:'Invalid input'
+            });
+        }else{
+            let user = await User.findOne({email});
+            if(!user){
+                res.status(400).json({
+                    success:false,
+                    error:'User does not exist'
+                });
+            }else{
+
+                bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(!isMatch) return res.status(400).json({
+                        success:false,
+                        error:'Wrong credentials'
+                    });
+                
+                                jwt.sign({
+                                    id:user._id
+                                }, process.env.JWT_SECRET,{
+                                    expiresIn:3600
+                                }, (err, token) => {
+                                console.log(err);
+                                res.status(201).json({
+                                success:true,
+                                token:token,
+                                data:{
+                                    id:user._id,
+                                    name:user.name,
+                                    email:user.email
+                                }
+                            })
+                                   
+                                });
+                            })
             }
         }
     } catch (error) {
